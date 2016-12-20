e8641a13821d04e126876a5afbdd6cb7756188a8
Neos21 <neos21@gmail.com>
2016-12-21 00:06:21 +0900

    08 Projects の一覧を表示させよう

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,10 @@
 class ProjectsController < ApplicationController
+  
+  # IndexAction を作る
+  def index
+    # @projects は View で使用する変数
+    # Project.all で全件取得
+    @projects = Project.all
+  end
+  
 end
