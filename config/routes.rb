e6cc817e92a62129c7f857ccd025fc0d19b913b2
Neos21 <neos21@gmail.com>
2016-12-21 00:12:41 +0900

    09 Root の設定をしよう

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # プロジェクトのルーティングを自動生成させる
   resources :projects
   
+  # ルートのアクションを対応付ける
+  root 'projects#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
