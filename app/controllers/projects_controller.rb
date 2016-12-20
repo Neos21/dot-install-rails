9ca296823376bdf1fb04db67fe2f27204bf03010
Neos21 <neos21@gmail.com>
2016-12-21 00:56:10 +0900

    11 Projects の詳細を表示しよう

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,11 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
   
+  # ShowAction
+  def show
+    # URL で渡された値は params[シンボル] で取得可能
+    # シンボル :id は rake routes で確認できる URI Pattern にて定義されている
+    @project = Project.find(params[:id])
+  end
+  
 end
