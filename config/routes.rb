f631e2c84c56f4a8812c0994007f60c4e4365c0b
Neos21 <neos21@gmail.com>
2016-12-20 23:53:48 +0900

    07 Controller を作ってみよう

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  # プロジェクトのルーティングを自動生成させる
+  resources :projects
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
