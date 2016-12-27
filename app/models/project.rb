f16730bf97a0a10068810f2c953198bb1f1f6275
Neos21 <neos21@gmail.com>
2016-12-28 02:18:43 +0900

    15 エラーメッセージを表示しよう

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   
-  # Title を必須入力とする Validation
-  validates :title, presence: true
+  # Title の Validation
+  validates :title,
+    presence: { message: "入力してください。" },      # 必須入力
+    length:   { minimum: 3, message: "短すぎます。"}  # 3文字以上 (全角も1文字とみなしてくれる)
   
 end
