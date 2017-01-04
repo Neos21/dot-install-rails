76a5fe9c3d021d86df3420062dc761969b6afb6d
Neos21 <neos21@gmail.com>
2017-01-05 00:18:24 +0900

    23 Tasks を保存していこう

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  
+  # CreateAction
+  def create
+    # 新規登録する Task に紐付く Project を取得する
+    @project = Project.find(params[:project_id])
+    
+    # パラメータを基に Task を作る
+    @task = @project.tasks.create(task_params)
+    
+    # Project の詳細画面を表示する
+    redirect_to project_path(@project.id)
+  end
+  
+  private
+    
+    # 受け取るパラメータを private で定義しておく
+    def task_params
+      params[:task].permit(:title)
+    end
+  
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+  
+  # Title の空欄登録を許容しない
+  validates :title, presence: true
 end
